6e46ee63fbf500a0b6527a02c7a509c36fa797ee
Taylor Thompson <ttaythompson@gmail.com>
2019-08-20 12:38:15 -0400

    Created a function to find the required modules and a callback function

--- fileLocator.js
+++ fileLocator.js
@@ -1,6 +1,9 @@
+/* eslint-disable quotes */
 const fs = require("fs");
 
-const locateDirectoryFiles = directoryPath => {
+// function will return the files found in directory based on project path
+
+const locateDirectoryFiles = (directoryPath, callback) => {
   const filesInDirectory = fs.readdirSync(directoryPath, function(
     error,
     files
@@ -9,9 +12,44 @@ const locateDirectoryFiles = directoryPath => {
       console.log("Error locating files in directory: ", error);
     } else {
       console.log("These are the files in the project directory", files);
+      callback(null, files);
     }
   });
+
   return filesInDirectory;
 };
 
-locateDirectoryFiles("/Users/taylorthompson/vim-city");
+const callbackFunc = (error, content) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("content", content);
+  }
+};
+
+locateDirectoryFiles("/Users/taylorthompson/vim-city", callbackFunc);
+
+//function that will return the modules needed for project found in the package.json file
+
+const requiredModules = (directoryPath, callback) => {
+  const filesFound = locateDirectoryFiles(directoryPath);
+
+  const packageJson = filesFound.filter(file => file === "package.json");
+
+  const packageName = packageJson.join();
+
+  const modules = fs.readFile(`${directoryPath}/${packageName}`, function(
+    error,
+    data
+  ) {
+    if (error) {
+      console.error(error);
+    } else {
+      let moduleObject = JSON.parse(data);
+      callback(null, moduleObject.dependencies);
+    }
+  });
+  return modules;
+};
+
+requiredModules("/Users/taylorthompson/vim-city", callbackFunc);
